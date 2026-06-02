529378a1f3042d00c88906daff98f060c2467633
MAvishkar12 <moreavishkar12@gmail.com>
2026-06-02 14:23:08 +0530

    Add headers in delete user api

--- tests/GetData.spec.js
+++ tests/GetData.spec.js
@@ -3,10 +3,15 @@ const url = "https://restful-booker.herokuapp.com/apidoc/#api-Booking-GetBooking
 test("Check the api testing", async ({ request }) => {
     const response = await request.get("https://restful-booker.herokuapp.com/booking/1")
     const data = await response.json()
+    console.log("data is",data)
+    if (response.status() === 404) {
+        console.log("Booking not found");
+        return; // stop test execution
+    }
     expect(response.status()).toBe(200)
     console.log(response.ok())
     expect(response.ok()).toBeTruthy()
     expect(data).toHaveProperty('firstname')
-     expect(data.firstname).toBeTruthy()
-  
+    expect(data.firstname).toBeTruthy()
+
 })
